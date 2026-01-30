fc4b615d7318346fe0c56c7a70003817a13d7f06
= <=>
2026-01-30 10:32:02 +0530

    Second test

--- src/main/java/org/adarsh/devopslab05/Controller/TestController.java
+++ src/main/java/org/adarsh/devopslab05/Controller/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("hello")
     public String greetings(){
-        return "Hello World";
+        return "Hello World bhai";
     }
 }
